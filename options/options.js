c00c873d1ad4e173a3eebf08dfbb2848fb601771
Crowdbotics <team@crowdbotics.com>
2022-08-09 04:19:34 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "bgi_tower_install_36680",
+  url: "https://bgi_tower_install_36680.botics.co",
+  api: "https://bgi_tower_install_36680.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
